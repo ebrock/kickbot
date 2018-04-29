8d29ebbdf7690757e15cb59ba404575c40f19947
eric <ebrockk@gmail.com>
2018-04-29 10:44:01 -0400

    removed config.py file

--- config/config.py
+++
@@ -1,3 +0,0 @@
-prod = "NDIzNjU0Nzk2NTg3ODI3MjAw.DYthUA.RFjI8ZcYlqk5El-_K2fvgkF4OPY"
-test = "NDI5NDc1MzQ4OTA0ODA0MzUz.DaCOGg.vAPFav-JnkRPhNbgeJmAXeb3txk"
-giphy_key = "OAPo1ZgCQU2fMEWGtm1y2UiAeAX7uJSK"
