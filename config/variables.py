209b558699f174ff2acffffa042eb2e7be88e028
Eric Brock <ebrockk@gmail.com>
2018-10-24 11:56:07 -0400

    more clean up

--- config/variables.py
+++ config/variables.py
@@ -1,9 +1,8 @@
-forbiddenWords = ['crap', 'dang', 'fuck', 'shit', 'pussy', 'cunt',
-                  'bitch', 'ass', 'damn', 'hell', 'balls', 'dick']
+forbiddenWords = ['crap', 'dang', 'damn', 'hell']
 
 ban_phrases = ['**{0.name}** HAS BEEN BANISHED TO THE SHADOW REALM!!!',
                '**{0.name}** has been crushed by Thor\'s mighty ban hammer!!!',
-               '**{0.name}** has been ejected for outstanding douchebaggery!!!',
+               '**{0.name}** has been ejected!!!',
                'NJ Chiefs raised the bar. **{0.name}** fell under it.',
                'No neckbeards allowed. That means you, **{0.name}**.']
 
@@ -14,7 +13,7 @@ emoji = [':fearful:',
          ':poop:']
 
 unban_phrases = ['Welcome back, **{0.mention}**. Have you learned your lesson?',
-                 '**{0.mention}** has returned. What a douche.',
-                 'Oh look, it\'s **{0.mention}**. Jackass.',
+                 '**{0.mention}** has returned.',
+                 'Oh look, it\'s **{0.mention}**.',
                  '{0.mention} has been unbanished. No one is happy about it.',
-                 'I feel a draft. Did {0.mention} forget to close the door again? Prick.']
+                 'I feel a draft. Did {0.mention} forget to close the door again?']
